f9a667e56f05dbacef9966c06520bcabb84fa534
Michel Glazenborg <mglazenborg@accessdenied.dev>
2026-05-12 21:57:17 +0200

    updated preflight

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -2,3 +2,5 @@
 
 source "$ELPHAEL_INSTALL/preflight/pacman.sh"
 source "$ELPHAEL_INSTALL/preflight/yay.sh"
+source "$ELPHAEL_INSTALL/preflight/first-run.sh"
+source "$ELPHAEL_INSTALL/preflight/disable-mkinitcpio.sh"
